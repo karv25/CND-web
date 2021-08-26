ba3c5bae9f975465d550fb3b6d1383cdf3a06ca0
elixirevo <elixirevocation@gmail.com>
2021-08-26 13:19:20 +0900

    add: event design

---
+++ src/components/Event/EventContent.svelte
@@ -0,0 +1,70 @@
+<script lang="ts">
+  export let progress: boolean
+  export let name: string
+  export let description: string
+  export let img: string
+</script>
+
+<div class="event-box">
+  <img class="event-img" src="{img}" alt="event-img" />
+  <div class="event-sub-box">
+    {#if progress}
+      <div class="event-mark">진행중</div>
+    {:else}
+      <div class="event-mark">종료</div>
+    {/if}
+    <div class="event-title">{name}</div>
+    <div class="event-content">
+      {description}
+    </div>
+  </div>
+</div>
+
+<style lang="scss">
+  .event-box {
+    width: 32%;
+    box-sizing: border-box;
+    border: 1px solid lightgray;
+    border-radius: 10px;
+    margin-bottom: 10px;
+  }
+
+  .event-sub-box {
+    margin: 10px;
+  }
+
+  .event-mark {
+    width: fit-content;
+    padding: 3px;
+    background-color: $highlight-color;
+    border-radius: 3px;
+    margin-bottom: 5px;
+    font-size: 0.8rem;
+  }
+
+  .event-title {
+    font-size: 1.3rem;
+    margin-bottom: 10px;
+  }
+
+  .event-content {
+    width: 100%;
+    font-size: 0.9rem;
+    line-height: 1rem;
+  }
+
+  .event-img {
+    width: 100%;
+    border-radius: 9px 9px 0px 0px;
+  }
+
+  @media screen and (max-width: 768px) {
+    .event-box {
+      width: 100%;
+      box-sizing: border-box;
+      border: 1px solid lightgray;
+      border-radius: 10px;
+      margin-bottom: 10px;
+    }
+  }
+</style>
--- src/components/Event/index.svelte
+++ src/components/Event/index.svelte
@@ -1,10 +1,44 @@
 <script lang="ts">
+  import EventContent from './EventContent.svelte'
 
+  interface EContent {
+    progress: boolean
+    name: string
+    description: string
+    img: string
+  }
+
+  const eventData: Array<EContent> = [
+    {
+      progress: true,
+      name: '에어드랍 이벤트',
+      description:
+        '에어드랍 이벤트! 우리 트위터를 팔로우 하고 트윗을 남겨주세요! 300명을 선정해서 CxNxD V2 를 에어드랍 합니다!',
+      img: 'https://dummyimage.com/200x100.gif?text=event-sample'
+    },
+    {
+      progress: true,
+      name: '에어드랍 이벤트',
+      description:
+        '에어드랍 이벤트! 우리 트위터를 팔로우 하고 트윗을 남겨주세요! 300명을 선정해서 CxNxD V2 를 에어드랍 합니다!',
+      img: 'https://dummyimage.com/200x100.gif?text=event-sample'
+    },
+    {
+      progress: true,
+      name: '에어드랍 이벤트',
+      description:
+        '에어드랍 이벤트! 우리 트위터를 팔로우 하고 트윗을 남겨주세요! 300명을 선정해서 CxNxD V2 를 에어드랍 합니다!',
+      img: 'https://dummyimage.com/200x100.gif?text=event-sample'
+    }
+  ]
 </script>
 
 <div class="container">
   <div class="container-title">Event</div>
   <div class="container-content">
+    {#each eventData as item}
+      <EventContent progress="{item.progress}" name="{item.name}" description="{item.description}" img="{item.img}" />
+    {/each}
   </div>
 </div>
 
@@ -15,7 +49,7 @@
     display: flex;
     flex-wrap: wrap;
     width: 100%;
-    justify-content: center;
+    justify-content: space-between;
     align-items: center;
   }
 </style>
