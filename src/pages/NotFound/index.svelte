9663f71f1bbd58deff411b5ef7178e398d8d66ff
elixirevo <elixirevocation@gmail.com>
2021-09-10 02:29:01 +0900

    update: Not found page

--- src/pages/NotFound/index.svelte
+++ src/pages/NotFound/index.svelte
@@ -1,3 +1,32 @@
-<h2>NotFound</h2>
+<div class="space">
+  <img class="nf-img" src="/assets/v2-sample.jpg" alt="sample" />
+  <div class="nf-title"><b>Not Found</b></div>
+  <p>Oops, this route doesn't exist!</p>
+  <a class="nf-link" href="/#/">Go to the main page.</a>
+</div>
 
-<p>Oops, this route doesn't exist!</p>
+<style lang="scss">
+  .nf-img {
+    width: 250px;
+    border-radius: 10px;
+    margin-bottom: 30px;
+  }
+
+  .nf-title {
+    color: $highlight-color;
+    font-size: 1.3rem;
+    margin-bottom: 10px;
+  }
+
+  .nf-link {
+    color: $highlight-color;
+    margin-top: 10px;
+  }
+  .space {
+    height: 80vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+</style>
